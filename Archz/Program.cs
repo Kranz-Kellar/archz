fec337cf0fcf0c61a9f748e20d7f83e5096342de
kranz-kellar <kranz.kellar@gmail.com>
2020-01-20 13:37:19 +0300

    Добавьте файлы проекта.

---
+++ Archz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Archz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Archz/core/Module.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archz.core
+{
+
+    public interface Module
+    {
+        void Init();
+        void Start();
+        void Update();
+        void Disable();
+        void Enable();
+        void Terminate();
+    }
+}
