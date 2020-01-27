6be759e5966c8b4c838734fc06d282a80d61be21
kranz-kellar <kranz.kellar@gmail.com>
2020-01-27 17:26:44 +0300

    Trying to create own ConsoleUI

--- Archz/core/Archz.cs
+++ Archz/core/Archz.cs
@@ -8,36 +8,63 @@ namespace Archz.core
     public class Archz
     {
         private List<Module> modulesList;
+        private ConsoleUI consoleUI;
+        private bool isOnline;
 
         public void Run()
         {
-            Init();
-            Start();
-            Update();
-            Terminate();
+            try
+            {
+                //Init();
+                //Start();
+                //Update();
+                //Terminate();
+
+               
+                
+            }
+            catch(Exception e)
+            {
+                Logger.Log(LogStatus.ERROR, $"Error was occured \"{e.Message}\"");
+            }
+
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+            consoleUI = new ConsoleUI();
+            var textArea = new ConsoleTextArea(0, 0, Console.WindowWidth / 3 * 2, Console.WindowHeight - 1, '+', ConsoleColor.Red, ConsoleColor.White);
+            textArea.WriteText("test\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\nsomeImportantInfo\n");
+            //consoleUI.RenderTextArea(textArea);
+            textArea.Render();
         }
 
         private void Init()
         {
-            Logger.Log(LogStatus.INFO, $"SESSION STARTED =============================================");
+            Logger.Log(LogStatus.INFO, $"SESSION INIT =============================================");
             SettingsManager.Init();
             modulesList = ModuleReflector.GetAllModules();
             modulesList.ForEach(x => x.Init());
+            //isOnline = true;
+            Logger.Log(LogStatus.INFO, $"SESSION INIT COMPLETE ====================================");
         }
 
         private void Start()
         {
             modulesList.ForEach(x => x.Start());
+            Logger.Log(LogStatus.INFO, $"SESSION STARTED ===========================================");
         }
 
         private void Update()
         {
-            modulesList.ForEach(x => x.Update());
+            while (isOnline)
+            {
+                modulesList.ForEach(x => x.Update());
+            }
         }
 
         private void Terminate()
         {
             modulesList.ForEach(x => x.Terminate());
+            isOnline = false;
             Logger.Log(LogStatus.INFO, $"SESSION TERMINATED ==========================================");
         }
     }
---
+++ Archz/core/consoleUI/ConsoleUIComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archz.core
+{
+    public class ConsoleUIComponent
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsSelected { get; set; }
+
+        public ConsoleUIComponent(int x, int y, int width, int height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+            IsEnabled = true;
+        }
+    }
+}
